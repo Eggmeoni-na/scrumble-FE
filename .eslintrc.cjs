35bf51ae2672a47719aa22287d94f6e8ced046b3
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-08 19:32:39 +0900

    #1 Chore: ESLint 및 Prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
