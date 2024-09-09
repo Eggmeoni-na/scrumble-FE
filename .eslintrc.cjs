0c9046691c1f39fc3f033e362e1febb4d296104d
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-09 20:26:21 +0900

    #1 Chore: 절대경로 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    'prettier/prettier': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
