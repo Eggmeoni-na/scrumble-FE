ff1e87f941a73af08d9951f27a90de5bb48e927b
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-12 20:42:29 +0900

    feat: path에 따른 라우팅 설정 및 페이지 컴포넌트 추가 (#14)
    * #4 feat: 라우팅 설정 및 페이지 컴포넌트 추가
    
    * #4 chore: react-router-dom 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { globalStyles } from '@/styles/globalStyles';
 import { Global } from '@emotion/react';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
       <Global styles={globalStyles} />
+      <Outlet />
     </>
   );
 }
