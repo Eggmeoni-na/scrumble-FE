49ef748369f909c33e09453b6a9da5fb5a753f47
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-16 01:09:03 +0900

    Feat: MSW 설정 및 테스트용 기본 handler 작성 (#16)
    * #8 feat: MSW 기본 설정 및 test handler 작성
    
    * #8 chore: MSW 설치 및 gitignore에 env 옵션 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import { initMocks } from '@/mocks/initMocks';
 import { router } from '@/Router/router';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+initMocks().then(() => {
+  createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+});
