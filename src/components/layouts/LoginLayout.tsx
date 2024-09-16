888fc724aeb04bb88ef782c41291fbbd8ad594d2
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-17 00:46:55 +0900

    Feat: 로그인 페이지 UI 구현 (#18)
    * #10 refactor: 미디어쿼리 함수로 개선
    
    * #10 feat: 로그인 페이지 UI 구현 및 레이아웃 부분 적용
    
    * #10 chore: 로그인 페이지에 사용된 이미지 정적 파일 추가
    
    * #10 refactor: 기존에 적용된 미디어쿼리 로직을 변경된 함수로 개선

---
+++ src/components/layouts/LoginLayout.tsx
@@ -0,0 +1,17 @@
+import Container from '@/components/layouts/Container';
+import Main from '@/components/layouts/Main';
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+const LoginLayout = ({ children }: Props) => {
+  return (
+    <Container>
+      <Main>{children}</Main>
+    </Container>
+  );
+};
+
+export default LoginLayout;
