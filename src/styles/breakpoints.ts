b935e7cadf807a8d9e90d8b9c7817b536a024f8a
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-17 00:30:56 +0900

    #10 refactor: 미디어쿼리 함수로 개선

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,4 +1,18 @@
+import { css, SerializedStyles } from '@emotion/react';
+
 export const breakpoints = {
   mobile: '375px',
   pc: '600px',
 };
+
+export const MobileMediaQuery = (styles: SerializedStyles) => css`
+  @media (min-width: ${breakpoints.mobile}) {
+    ${styles}
+  }
+`;
+
+export const pcMediaQuery = (styles: SerializedStyles) => css`
+  @media (min-width: ${breakpoints.pc}) {
+    ${styles}
+  }
+`;
