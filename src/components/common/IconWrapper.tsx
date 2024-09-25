93b23ab89ad159d1978cdf5ab22f4d764123b0b8
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-25 17:51:03 +0900

    Feat: Footer 컴포넌트 UI 구현 및 라우트 연결 (#31)
    * #23 feat: Footer 네비게이션 아이콘 추가
    
    * fix: globalStyles 기본 font 속성 제거
    
    * design: IconWrapper 필요없는 스타일 속성 제거
    
    * #23 design: a태그 기본 스타일 제거
    
    * #23 feat: Footer 컴포넌트 구현
    
    * chore: gitignore vercel 추가

--- src/components/common/IconWrapper.tsx
+++ src/components/common/IconWrapper.tsx
@@ -11,25 +11,19 @@ type Props = {
 
 type IconProps = {
   disabled?: boolean;
-} & { theme: Theme };
+};
 
 const IconWrapper = ({ children, onClick, name, disabled = false, customStyle }: Props) => (
-  <div
-    css={(theme) => [customStyle ? customStyle : icon({ disabled, theme })]}
-    onClick={onClick}
-    aria-label={name}
-    role="icon"
-  >
+  <div css={[customStyle ? customStyle : icon({ disabled })]} onClick={onClick} aria-label={name} role="icon">
     {children}
   </div>
 );
 export default IconWrapper;
 
-const icon = ({ disabled, theme }: IconProps) => css`
+const icon = ({ disabled }: IconProps) => css`
   width: 36px;
   height: 36px;
   padding: 4px;
-  stroke: ${theme.colors.text};
   stroke-width: 0.1;
   visibility: ${disabled ? 'hidden' : 'visible'};
   transition: all 0.3s ease-in-out;
