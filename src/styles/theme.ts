2369a914d55092928c806b57da672e2c06ed01bf
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-25 15:43:24 +0900

    #29 refactor: globalStyles typography 추가 및 타입 관리 방법 개선

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,37 +1,12 @@
-import { Theme } from '@emotion/react';
+import { darkColors, lightColors } from '@/styles/palette';
+import { typography } from '@/styles/typography';
 
-export const lightTheme: Theme = {
-  colors: {
-    primary: '#FFA500',
-    secondary: '#FFD700',
-    background: {
-      white: '#ffffff',
-      yellow: '#FFFACD',
-      lightYellow: '#FFFEF6',
-    },
-    text: '#24272B',
-    border: {
-      gray100: '#e0e0e0',
-      gray200: '#bdbdbd',
-      gray300: '#9e9e9e',
-    },
-  },
+export const lightTheme = {
+  colors: lightColors,
+  typography,
 };
 
-export const darkTheme: Theme = {
-  colors: {
-    primary: '#FFA500',
-    secondary: '#FFD700',
-    background: {
-      white: '#1B1C1D',
-      yellow: '#34373B',
-      lightYellow: '#24272B',
-    },
-    text: '#ffffff',
-    border: {
-      gray100: '#7F7F7F',
-      gray200: '#B1B1B1',
-      gray300: '#C9C9C9',
-    },
-  },
+export const darkTheme = {
+  colors: darkColors,
+  typography,
 };
