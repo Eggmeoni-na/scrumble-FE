1b33cf08231e84e42051dbbab9860a6d3f6b8e17
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-02 20:39:47 +0900

    Refactor: 웹 접근성을 위한 IconWrapper 컴포넌트 개선 (#41)
    * #40 refactor: 웹 접근성을 위한 IconWrapper 컴포넌트 개선
    
    * #40 refactor: 속성명 변경에 따른 관련 컴포넌트 수정
    
    * #40 refactor: IconWrapper에 rest parameter 적용으로 확장성 개선
    
    * #40 refactor: 관련 컴포넌트 속성 수정

--- src/components/common/IconWrapper.tsx
+++ src/components/common/IconWrapper.tsx
@@ -1,26 +1,31 @@
 import { css, Theme } from '@emotion/react';
-import { ReactNode } from 'react';
+import { ElementType, HTMLAttributes, ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
   onClick?: () => void;
-  name: string;
   disabled?: boolean;
   customStyle?: (theme: Theme) => ReturnType<typeof css>;
-};
+} & HTMLAttributes<HTMLElement>;
 
 type IconProps = {
   disabled?: boolean;
 };
 
-const IconWrapper = ({ children, onClick, name, disabled = false, customStyle }: Props) => (
-  <div css={[customStyle ? customStyle : icon({ disabled })]} onClick={onClick} aria-label={name} role="icon">
-    {children}
-  </div>
-);
+const IconWrapper = ({ children, onClick, disabled = false, customStyle, ...rest }: Props) => {
+  const Wrapper: ElementType = onClick ? 'button' : 'div';
+  return (
+    <Wrapper css={[customStyle ? customStyle : icon({ disabled })]} onClick={onClick ? onClick : undefined} {...rest}>
+      {children}
+    </Wrapper>
+  );
+};
 export default IconWrapper;
 
 const icon = ({ disabled }: IconProps) => css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 36px;
   height: 36px;
   padding: 4px;
@@ -31,5 +36,10 @@ const icon = ({ disabled }: IconProps) => css`
   :hover {
     transform: scale(1.1);
   }
-  cursor: pointer;
+
+  svg {
+    width: 100%;
+    height: 100%;
+    transition: inherit;
+  }
 `;
