f60505cce0581e02692dea0de78112b45e0cd9e6
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-02 20:09:32 +0900

    #40 refactor: IconWrapper에 rest parameter 적용으로 확장성 개선

--- src/components/common/IconWrapper.tsx
+++ src/components/common/IconWrapper.tsx
@@ -1,29 +1,21 @@
 import { css, Theme } from '@emotion/react';
-import { ElementType, ReactNode } from 'react';
+import { ElementType, HTMLAttributes, ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
   onClick?: () => void;
-  role?: 'button' | 'icon';
-  label: string;
-
   disabled?: boolean;
   customStyle?: (theme: Theme) => ReturnType<typeof css>;
-};
+} & HTMLAttributes<HTMLElement>;
 
 type IconProps = {
   disabled?: boolean;
 };
 
-const IconWrapper = ({ children, onClick, label, role = 'button', disabled = false, customStyle }: Props) => {
-  const Wrapper: ElementType = role === 'button' ? 'button' : 'div';
+const IconWrapper = ({ children, onClick, disabled = false, customStyle, ...rest }: Props) => {
+  const Wrapper: ElementType = onClick ? 'button' : 'div';
   return (
-    <Wrapper
-      css={[customStyle ? customStyle : icon({ disabled })]}
-      onClick={role === 'button' && onClick ? onClick : undefined}
-      aria-label={label}
-      role={role}
-    >
+    <Wrapper css={[customStyle ? customStyle : icon({ disabled })]} onClick={onClick ? onClick : undefined} {...rest}>
       {children}
     </Wrapper>
   );
@@ -34,9 +26,8 @@ const icon = ({ disabled }: IconProps) => css`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  all: unset;
-  width: 28px;
-  height: 28px;
+  width: 36px;
+  height: 36px;
   padding: 4px;
   stroke-width: 0.1;
   transition: all 0.3s ease-in-out;
@@ -45,7 +36,6 @@ const icon = ({ disabled }: IconProps) => css`
   :hover {
     transform: scale(1.1);
   }
-  cursor: pointer;
 
   svg {
     width: 100%;
