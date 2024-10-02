745a55426d03b3d4a557e91ebbbe8dea3ebed6bc
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-02 14:18:32 +0900

    #37 feat: Root에 ModalProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { ToastContainer } from '@/components/common/Toast';
+import { ModalProvider } from '@/context/ModalContext';
 import { useThemeStore } from '@/stores/theme';
 import { darkTheme, globalStyles, lightTheme } from '@/styles';
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
-import './App.css';
 
 const queryClient = new QueryClient();
 
 function App() {
   const isDarkMode = useThemeStore((state) => state.isDarkMode);
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        <Global styles={globalStyles} />
-        <Outlet />
-        <ToastContainer />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ModalProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+          <Global styles={globalStyles} />
+          <Outlet />
+          <ToastContainer />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </ModalProvider>
   );
 }
 
