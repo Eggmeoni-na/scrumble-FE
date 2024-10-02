6f508f443a3eb489b92e85964c121134b1c3644a
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-02 14:24:19 +0900

    Feat: 전역 모달 로직과 템플릿 UI 재사용 및 스타일링 확장성 개선 (#39)
    * #37 feat: 전역 모달 로직 구현
    
    * #37 feat: useModal hook 추가
    
    * #37 feat: Overlay 컴포넌트 구현
    
    * #37 feat: 모달 콘텐츠 UI 컴포넌트 모듈화
    
    * #37 design: 전역 모달 스타일 글로벌 파일에 작성
    
    * #37 feat: Root에 ModalProvider 설정

---
+++ src/components/common/Overlay/Overlay.tsx
@@ -0,0 +1,40 @@
+import { css } from '@emotion/react';
+import { PropsWithChildren } from 'react';
+
+export type OverlayProps = {
+  onClose?: () => void;
+  preventClick?: boolean;
+  transparent?: boolean;
+};
+
+const Overlay = ({ children, onClose, preventClick = true, transparent = false }: PropsWithChildren & OverlayProps) => {
+  return (
+    <div
+      css={[container, transparent && bgTransparent]}
+      onClick={preventClick ? undefined : onClose}
+      onKeyDown={() => {}}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Overlay;
+
+const container = css`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(0 0 0 / 35%);
+  backdrop-filter: blur(5px);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const bgTransparent = css`
+  background: transparent;
+  backdrop-filter: blur(5px);
+`;
