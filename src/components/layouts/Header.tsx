c9001ab6fb602ef5275e769d0aa826a86914b704
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-04 19:17:27 +0900

    #33 feat: 경로별 Header 아이콘 숨김 처리

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,14 +2,25 @@ import { Add, Back, Bell, Dark, Light } from '@/assets/icons';
 import IconWrapper from '@/components/common/IconWrapper';
 import { useThemeStore } from '@/stores/theme';
 import { css, Theme } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const { isDarkMode, toggleTheme } = useThemeStore((state) => state);
+  const navigate = useNavigate();
+
+  const handleBack = () => navigate(-1);
+  const handleCreateSquad = async () => {
+    const squadName = window.prompt('스쿼드명을 입력하세요.');
+    if (!squadName) return;
+  };
+
   return (
     <header css={headerContainer}>
-      <IconWrapper aria-label="Go to back" role="button">
-        <Back />
-      </IconWrapper>
+      <div>
+        <IconWrapper aria-label="Go to back" role="button" onClick={handleBack} disabled={location.pathname === '/'}>
+          <Back />
+        </IconWrapper>
+      </div>
       <div css={rightMenu}>
         <IconWrapper aria-label="Toggle theme" onClick={toggleTheme} role="button">
           {isDarkMode ? <Dark /> : <Light />}
@@ -17,7 +28,12 @@ const Header = () => {
         <IconWrapper aria-label="alarm" role="button">
           <Bell />
         </IconWrapper>
-        <IconWrapper aria-label="Add squad" role="button">
+        <IconWrapper
+          aria-label="Add squad"
+          role="button"
+          onClick={handleCreateSquad}
+          disabled={location.pathname === '/'}
+        >
           <Add />
         </IconWrapper>
       </div>
