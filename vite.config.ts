5abe45f67aedbcf25350c25637c96a89b528c644
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-08 15:14:51 +0900

    chore: vite.config.ts proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
-    tsconfigPaths(),
-    svgr(),
-  ],
-  server: {
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: '',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+      }),
+      tsconfigPaths(),
+      svgr(),
+    ],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
