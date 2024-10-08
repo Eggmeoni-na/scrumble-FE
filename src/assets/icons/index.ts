4edfaf9bde8bf8a02c8e4f38023f8ebb18fd84e2
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-08 15:15:53 +0900

    feat: 아이콘 파일 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,11 @@
 import Add from './add.svg?react';
 import Back from './back.svg?react';
 import Bell from './bell.svg?react';
+import Check from './check.svg?react';
 import Close from './close.svg?react';
 import Dark from './dark.svg?react';
+import Edit from './edit.svg?react';
+import Exit from './exit.svg?react';
 import Failed from './failed.svg?react';
 import Light from './light.svg?react';
 import Menu from './menu.svg?react';
@@ -12,4 +15,4 @@ import Target from './target.svg?react';
 import User from './user.svg?react';
 import Warning from './warning.svg?react';
 
-export { Add, Back, Bell, Close, Dark, Failed, Light, Menu, Pouch, Success, Target, User, Warning };
+export { Add, Back, Bell, Check, Close, Dark, Edit, Exit, Failed, Light, Menu, Pouch, Success, Target, User, Warning };
