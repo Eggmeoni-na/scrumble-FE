c7f3c0ac5aa12c889c4d11b9b61bb83b87f62c22
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-08 15:15:35 +0900

    chore: 기본 ErrorBoundary 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useThemeStore } from '@/stores/theme';
 import { darkTheme, globalStyles, lightTheme } from '@/styles';
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
 
 const queryClient = new QueryClient();
@@ -11,15 +12,17 @@ const queryClient = new QueryClient();
 function App() {
   const isDarkMode = useThemeStore((state) => state.isDarkMode);
   return (
-    <ModalProvider>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-          <Global styles={globalStyles} />
-          <Outlet />
-          <ToastContainer />
-        </ThemeProvider>
-      </QueryClientProvider>
-    </ModalProvider>
+    <ErrorBoundary fallback={<h1>에러났슈</h1>}>
+      <ModalProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+            <Global styles={globalStyles} />
+            <Outlet />
+            <ToastContainer />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </ModalProvider>
+    </ErrorBoundary>
   );
 }
 
