673666e14b29d63153feca1788ed40419d173916
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-08 14:55:39 +0900

    #45 feat: 스쿼드 디테일 페이지 조회 API 연동

--- src/Pages/SquadDetailPage.tsx
+++ src/Pages/SquadDetailPage.tsx
@@ -1,4 +1,14 @@
+import { squadDetailQueryOptions } from '@/hooks/queries/useSquad';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
 const SquadDetailPage = () => {
+  const { squadId } = useParams();
+  const { data: squadDetail } = useSuspenseQuery({
+    ...squadDetailQueryOptions(Number(squadId)),
+    staleTime: 300000,
+  }).data;
+
   return <div>SquadDetailPage</div>;
 };
 
