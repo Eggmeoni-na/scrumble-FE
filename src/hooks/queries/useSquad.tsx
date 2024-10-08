b11d3a760b3c11312b53fc5e48bf89e12f3bb238
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-08 14:55:14 +0900

    #45 feat: 스쿼드 디테일 조회 queryOptions 정의

---
+++ src/hooks/queries/useSquad.tsx
@@ -0,0 +1,19 @@
+import { getSquadDetail, getSquadList } from '@/apis';
+import { queryOptions } from '@tanstack/react-query';
+
+export const squadKeys = {
+  squad: ['squadList'] as const,
+  squadDetail: (squadId: number) => [squadKeys.squad, squadId] as const,
+};
+
+export const squadQueryOptions = () =>
+  queryOptions({
+    queryKey: squadKeys.squad,
+    queryFn: getSquadList,
+  });
+
+export const squadDetailQueryOptions = (squadId: number) =>
+  queryOptions({
+    queryKey: squadKeys.squadDetail(squadId),
+    queryFn: () => getSquadDetail(squadId),
+  });
