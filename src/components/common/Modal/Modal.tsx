0b12782aefac4352a29ce2dcb5d15c00cbf38d1a
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-10 18:23:59 +0900

    fix: Modal 컴포넌트 내 필요없는 로직 제거

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,21 +1,8 @@
 import { ModalParameters, ModalType } from '@/types';
-import { memo, useEffect } from 'react';
+import { memo } from 'react';
 
 const Modal = ({ modal }: { modal: ModalType<ModalParameters> }) => {
   const { element: ModalContainer, resolve, reject, actionModal, props } = modal;
-
-  useEffect(() => {
-    const scrollY = window.scrollY;
-    document.body.style.cssText = `
-      position: fixed;
-      top: -${scrollY}px;
-      overflow-y: hidden;
-      width: 100%;`;
-    return () => {
-      document.body.style.cssText = '';
-      window.scrollTo(0, scrollY);
-    };
-  }, []);
 
   return <ModalContainer onSubmit={resolve} onAbort={reject} actionModal={actionModal} {...props} />;
 };
