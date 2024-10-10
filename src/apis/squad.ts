921f4e4d93708d8e2b963047c4a17c4aa45c9d15
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-10 20:09:14 +0900

    #46 feat: 스쿼드명 수정 api 작성

--- src/apis/squad.ts
+++ src/apis/squad.ts
@@ -1,5 +1,6 @@
 import { instance } from '@/apis';
-import { createSquadParamType } from '@/hooks/mutations/useCreateSquad';
+
+import { createSquadParamType, updateSquadNameParamType } from '@/hooks/mutations/useSquad';
 import { ApiResponse, Squad, SquadDetail } from '@/types';
 import { MutationFunction } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
@@ -16,5 +17,13 @@ export const createSquad: MutationFunction<ApiResponse<Squad>, createSquadParamT
 
 export const getSquadDetail = async (squadId: number): Promise<{ data: SquadDetail }> => {
   const response = await instance.get(`/api/squads/${squadId}`);
+  return response.data;
+};
+
+export const updateSquadName: MutationFunction<ApiResponse<SquadDetail>, updateSquadNameParamType> = async ({
+  squadId,
+  squadName,
+}) => {
+  const response: AxiosResponse<ApiResponse<SquadDetail>> = await instance.put(`/api/squads/${squadId}`, { squadName });
   return response.data;
 };
