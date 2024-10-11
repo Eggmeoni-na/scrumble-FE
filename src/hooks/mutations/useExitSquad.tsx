f496b3d900f7e414372dcd7cfa69b554171d47bf
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-11 21:47:53 +0900

    #60 feat: 스쿼드 나가기 API 연동 및 로직 구현

---
+++ src/hooks/mutations/useExitSquad.tsx
@@ -0,0 +1,43 @@
+import { exitSquad } from '@/apis';
+import SquadActionWarning from '@/components/common/Modal/ModalContents/SquadActionWarning';
+import { ExitAndDeleteSquadNameParamType, MutateOptionsType } from '@/hooks/mutations';
+import { useModal } from '@/hooks/useModal';
+import { ApiResponse } from '@/types';
+import { useMutation } from '@tanstack/react-query';
+
+const useExitSquad = (
+  squadId: number,
+  hasMembers: boolean,
+  options: MutateOptionsType<ApiResponse<null>, ExitAndDeleteSquadNameParamType>,
+) => {
+  const { ModalContainer: ExitSquadModal, openModal } = useModal();
+  const { mutate: exitSquadMutate } = useMutation({
+    mutationFn: exitSquad,
+    ...options,
+  });
+
+  const handleSquadExit = async () => {
+    const checkInfo = {
+      type: 'confirm',
+      text: '확인',
+      message: '나가기 전에 리더 권한을 위임해주세요.',
+      displayCancel: false,
+    } as const;
+
+    const exitInfo = {
+      type: 'delete',
+      text: '나가기',
+      message: '정말로 스쿼드를 나가시겠어요?',
+      displayCancel: true,
+    } as const;
+
+    const res = await openModal(SquadActionWarning, undefined, hasMembers ? checkInfo : exitInfo);
+    if (res.ok && !hasMembers) {
+      exitSquadMutate({ squadId });
+    }
+  };
+
+  return { ExitSquadModal, handleSquadExit };
+};
+
+export default useExitSquad;
