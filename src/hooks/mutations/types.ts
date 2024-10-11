165a9470d97b888c87c3dcc99894ae4f27cca216
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-11 21:43:47 +0900

    #60 refactor: 중복되는 타입 재사용

--- src/hooks/mutations/types.ts
+++ src/hooks/mutations/types.ts
@@ -6,4 +6,4 @@ import { UseMutationOptions } from '@tanstack/react-query';
 export type MutateOptionsType<T, U, Context = unknown> = UseMutationOptions<T, Error, U, Context>;
 export type CreateSquadParamType = { squadName: string };
 export type UpdateSquadNameParamType = { squadId: number; squadName: string };
-export type DeleteSquadNameParamType = { squadId: number };
+export type ExitAndDeleteSquadNameParamType = { squadId: number };
