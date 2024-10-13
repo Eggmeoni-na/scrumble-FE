717f7ae029bb31fecd24f5a07cadf4f47844759d
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-13 23:40:12 +0900

    #65 feat: 더보기 아이콘 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -9,10 +9,29 @@ import Exit from './exit.svg?react';
 import Failed from './failed.svg?react';
 import Light from './light.svg?react';
 import Menu from './menu.svg?react';
+import More from './more.svg?react';
 import Pouch from './pouch.svg?react';
 import Success from './success.svg?react';
 import Target from './target.svg?react';
 import User from './user.svg?react';
 import Warning from './warning.svg?react';
 
-export { Add, Back, Bell, Check, Close, Dark, Edit, Exit, Failed, Light, Menu, Pouch, Success, Target, User, Warning };
+export {
+  Add,
+  Back,
+  Bell,
+  Check,
+  Close,
+  Dark,
+  Edit,
+  Exit,
+  Failed,
+  Light,
+  Menu,
+  More,
+  Pouch,
+  Success,
+  Target,
+  User,
+  Warning,
+};
