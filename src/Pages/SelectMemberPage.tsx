c4822bbd9af2d7b2c4475fa098cf6dbb438135e0
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-14 17:51:42 +0900

    refactor: 변수명 수정

--- src/Pages/SelectMemberPage.tsx
+++ src/Pages/SelectMemberPage.tsx
@@ -2,9 +2,9 @@ import SelectMemberList from '@/components/common/Member/SelectMemberList';
 import { useParams } from 'react-router-dom';
 
 const SelectMemberPage = () => {
-  const param = useParams();
+  const params = useParams();
 
-  return <SelectMemberList squadId={Number(param.squadId)} />;
+  return <SelectMemberList squadId={Number(params.squadId)} />;
 };
 
 export default SelectMemberPage;
