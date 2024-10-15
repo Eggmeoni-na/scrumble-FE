ab8c584da2cb987ac0e583f745548d8cc7501f34
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-16 02:22:26 +0900

    Feat: Calendar 로직 구현 및 선택된 날짜의 투두리스트 조회 API 연동 (#74)
    * #73 chore: es-toolkit과 date-fns 설치
    
    * #73 feat: 월별 일자 계산 유틸 함수 작성
    
    * #73 feat: 선택된 날짜 상태 store 생성
    
    * #73 feat: 월간 이동 로직 구현 및 CalendarList 컴포넌트 분리

--- src/stores/todo.ts
+++ src/stores/todo.ts
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import { create } from 'zustand';
 
 type TodoState = {
@@ -8,4 +9,14 @@ type TodoState = {
 export const useTodoStore = create<TodoState>((set) => ({
   isTodoChanged: false,
   setIsTodoChanged: (changed) => set({ isTodoChanged: changed }),
+}));
+
+type DateState = {
+  selectedDay: string;
+  setSelectedDay: (date: string) => void;
+};
+
+export const useDayStore = create<DateState>((set) => ({
+  selectedDay: format(new Date(), 'yyyy-MM-dd'),
+  setSelectedDay: (day: string) => set({ selectedDay: day }),
 }));
