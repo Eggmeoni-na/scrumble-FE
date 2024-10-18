337cc38f08069e7d5e9fb8d17ca2ed7540ac2bc6
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-18 16:41:19 +0900

    fix: axios intance에 ngrok 설정 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -4,6 +4,7 @@ export const instance = axios.create({
   baseURL: import.meta.env.MODE === 'development' ? '' : import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': true,
   },
 });
 
