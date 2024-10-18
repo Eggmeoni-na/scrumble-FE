24f1b2b064ca87355eab6cff6f0d999a0370026e
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-18 18:02:16 +0900

    #69 feat: 투두 삭제를 위한 훅 구현

--- src/hooks/mutations/index.ts
+++ src/hooks/mutations/index.ts
@@ -3,6 +3,7 @@ export * from './types';
 export { default as useCreateSquad } from './useCreateSquad';
 export { default as useCreateTodo } from './useCreateTodo';
 export { default as useDeleteSquad } from './useDeleteSquad';
+export { default as useDeleteTodo } from './useDeleteTodo';
 export { default as useExitSquad } from './useExitSquad';
 export { default as useRemoveUserFromSquad } from './useRemoveUserFromSquad';
 export { default as useUpdateSquadName } from './useUpdateSquadName';
