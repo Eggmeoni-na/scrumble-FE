fae5cb5c656bff28e666319fcd700cf63b879bae
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-11-19 16:25:01 +0900

    #103 feat: Notification 컴포넌트 mock 데이터 기반으로 구현
    - 무한 스크롤 로직을 훅으로 분리
    - notificationHandler 작성

---
+++ src/hooks/queries/useNotification.tsx
@@ -0,0 +1,34 @@
+import { getNotificationsApi } from '@/apis/mockApi';
+import { infiniteQueryOptions } from '@tanstack/react-query';
+
+export const notiKeys = {
+  all: ['notifications'] as const,
+};
+
+const startDateTime = new Date().toISOString();
+const currentDate = new Date(startDateTime);
+const endDateTime = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000).toISOString();
+
+export const notificationInfiniteQueryOptions = () =>
+  infiniteQueryOptions({
+    queryKey: notiKeys.all,
+    queryFn: ({ pageParam }) =>
+      getNotificationsApi({
+        startDateTime,
+        endDateTime,
+        lastNotificationId: pageParam,
+        pageSize: 10,
+      }),
+    getNextPageParam: (lastPage) => {
+      if (!lastPage?.data || lastPage.data.length === 0) {
+        return null;
+      }
+      const lastToDoId = lastPage?.data[lastPage.data.length - 1].notificationId ?? null;
+      return lastToDoId;
+    },
+    initialPageParam: 0,
+    select: (data) => (data.pages ?? []).flatMap((page) => page.data),
+    refetchOnWindowFocus: false,
+  });
+
+export default notificationInfiniteQueryOptions;
