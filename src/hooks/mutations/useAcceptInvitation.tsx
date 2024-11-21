e5d947cd70af7e3ea0858659f0272a9a32dc7986
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-11-21 18:02:33 +0900

    #107 feat: 초대 수락을 위한 useMutation 로직 구현

---
+++ src/hooks/mutations/useAcceptInvitation.tsx
@@ -0,0 +1,15 @@
+import { acceptSquadInvite } from '@/apis';
+import { AcceptSquadInvitationParams, MutateOptionsType } from '@/hooks/mutations/types';
+import { ApiResponse } from '@/types';
+import { useMutation } from '@tanstack/react-query';
+
+const useAcceptInvitation = (options: MutateOptionsType<ApiResponse<null>, AcceptSquadInvitationParams>) => {
+  const { mutate: acceptInvitation } = useMutation({
+    mutationFn: acceptSquadInvite,
+    ...options,
+  });
+
+  return { acceptInvitation };
+};
+
+export default useAcceptInvitation;
