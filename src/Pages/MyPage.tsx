39802300584ec7851538d01c0559e3ed9140b7a6
kimjeonghee <93242226+k-jeonghee@users.noreply.github.com>
2024-11-22 18:01:03 +0900

    Feat: 마이페이지 UI 구현 및 유저 정보 조회 API 연동 (#116)
    - 유저 정보 조회 로직 추가
    - 스타일 적용

--- src/Pages/MyPage.tsx
+++ src/Pages/MyPage.tsx
@@ -1,5 +1,80 @@
+import { getUser } from '@/apis';
+import Button from '@/components/common/Button/Button';
+import { css, Theme } from '@emotion/react';
+import { useSuspenseQuery } from '@tanstack/react-query';
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  const { data: user } = useSuspenseQuery({
+    queryKey: ['me'],
+    queryFn: getUser,
+  }).data;
+
+  const handleLogout = () => {
+    // TODO: 로그아웃 API 연동
+  };
+
+  const handleDeleteUser = () => {
+    // TODO: 회원 탈퇴 API 연동
+  };
+
+  return (
+    <div css={containerStyle}>
+      <h1>내 정보</h1>
+      <div css={userInfoStyle}>
+        <p>{user.email}</p>
+        <Button text="로그아웃" onClick={handleLogout} aria-label="로그아웃" css={logoutButtonStyle} />
+      </div>
+      <div css={deleteUserButtonStyle}>
+        <span onClick={handleDeleteUser}>회원탈퇴</span>
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
+
+const containerStyle = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  margin: 0 32px;
+  padding: 24px 0;
+
+  & h1 {
+    font-size: ${theme.typography.size_24};
+    margin-bottom: 12px;
+  }
+`;
+
+const userInfoStyle = css`
+  display: flex;
+  justify-content: space-between;
+
+  & p {
+    flex: 2;
+    margin-top: 12px;
+  }
+`;
+
+const logoutButtonStyle = (theme: Theme) => css`
+  background-color: ${theme.colors.secondary};
+  color: white;
+
+  flex: 0.5;
+`;
+
+const deleteUserButtonStyle = (theme: Theme) => css`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  margin-top: auto;
+
+  & span {
+    color: ${theme.colors.gray.gray200};
+    cursor: pointer;
+
+    :hover {
+      opacity: 0.5;
+    }
+  }
+`;
