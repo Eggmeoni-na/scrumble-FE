f4a6bc3f5a4de56eb2956a25729689dcba829408
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-11-23 14:02:52 +0900

    #117 design: 보관함 임시 UI 구현

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,9 +1,31 @@
+import { css, Theme } from '@emotion/react';
+
 const HomePage = () => {
   return (
-    <>
-      <div>로그인 / 보관함</div>
-    </>
+    <div css={containerStyle}>
+      <span>🪹</span>
+      <p>보관함을 준비중이에요</p>
+    </div>
   );
 };
 
 export default HomePage;
+
+const containerStyle = (theme: Theme) => css`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & span {
+    display: inline-block;
+    font-size: 12rem;
+  }
+
+  & p {
+    ${theme.typography.size_24}
+    color: ${theme.colors.primary};
+    margin-bottom: 112px;
+  }
+`;
