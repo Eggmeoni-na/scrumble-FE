53c6a859464d68750e738e88b56561a9c34ad85e
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-11-23 14:02:20 +0900

    #117 refactor: 불필요한 경로 보호 컴포넌트 제거

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -2,7 +2,7 @@ import App from '@/App';
 import { MainLayout } from '@/components';
 import Loading from '@/components/common/Loading';
 import LoginLayout from '@/components/layouts/LoginLayout';
-import { HomePage, LoginPage, MyPage, ProtectedPage, SquadDetailPage, SquadPage } from '@/Pages';
+import { HomePage, LoginPage, MyPage, SquadDetailPage, SquadPage } from '@/Pages';
 import GoogleOAuthCallbackPage from '@/Pages/GoogleOAuthLoginPage';
 import InvitePage from '@/Pages/InvitePage';
 import NotFoundPage from '@/Pages/NotFoundPage';
@@ -26,11 +26,7 @@ export const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: (
-              <ProtectedPage>
-                <HomePage />
-              </ProtectedPage>
-            ),
+            element: <HomePage />,
           },
           {
             path: '/squads',
@@ -59,9 +55,9 @@ export const router = createBrowserRouter([
           {
             path: '/me',
             element: (
-              <ProtectedPage>
+              <Suspense fallback={<Loading />}>
                 <MyPage />
-              </ProtectedPage>
+              </Suspense>
             ),
           },
         ],
