d2d34a595b182a2873bc8f2fa74552c2b7b98298
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-12-08 14:54:34 +0900

    #136 refactor: 투두 수정 낙관적 업데이트 로직 onMutate에서 처리하도록 개선

--- src/hooks/mutations/useUpdateTodo.tsx
+++ src/hooks/mutations/useUpdateTodo.tsx
@@ -7,6 +7,7 @@ import { optimisticUpdateMutateHandler } from '@/utils';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useUpdateTodo = (
+  toDoId: number,
   squadId: number,
   selectedDay: string,
   options: MutateOptionsType<
@@ -18,10 +19,24 @@ export const useUpdateTodo = (
   const queryClient = useQueryClient();
   const { mutate: updateTodoMutate } = useMutation({
     mutationFn: updateTodo,
-    onMutate: async () => {
+    onMutate: async ({ newTodo }) => {
       const oldData = await optimisticUpdateMutateHandler<InfiniteQueryData<ApiResponse<ToDoDetail[]>>>(
         queryClient,
         todoKeys.todos(squadId, selectedDay),
+        (prevData: InfiniteQueryData<ApiResponse<ToDoDetail[]>>) => {
+          const todo = {
+            ...newTodo,
+            toDoId,
+          };
+
+          return {
+            ...prevData,
+            pages: prevData.pages.map((page) => ({
+              ...page,
+              data: page.data.map((prevTodo) => (prevTodo.toDoId === toDoId ? todo : prevTodo)),
+            })),
+          };
+        },
       );
 
       return { oldData };
