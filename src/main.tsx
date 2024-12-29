edeec64d1c1d323944f103e9ee136ee4ccef6fc5
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-12-29 16:57:52 +0900

    chore: MSW 모킹 초기화 로직 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import { initMocks } from '@/mocks/initMocks';
 import { router } from '@/Router/router';
 import { CookiesProvider } from 'react-cookie';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 
-initMocks().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <CookiesProvider defaultSetOptions={{ path: '/' }}>
-      <RouterProvider router={router} />
-    </CookiesProvider>,
-  );
-});
+createRoot(document.getElementById('root')!).render(
+  <CookiesProvider defaultSetOptions={{ path: '/' }}>
+    <RouterProvider router={router} />
+  </CookiesProvider>,
+);
