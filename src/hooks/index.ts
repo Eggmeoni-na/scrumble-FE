321d1016b13d9c5b14f29a177a13b564e684cde1
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-12-29 16:57:44 +0900

    chore: 사용하지 않는 훅 제거

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,5 @@ import { useModal } from '@/hooks/useModal';
 import useOpenToggle from '@/hooks/useOpenToggle';
 import useToastHandler from '@/hooks/useToastHandler';
 import useUserCookie from '@/hooks/useUserCookie';
-import useValidatedUser from '@/hooks/useValidatedUser';
 
-export { useInfinite, useModal, useOpenToggle, useToastHandler, useUserCookie, useValidatedUser };
+export { useInfinite, useModal, useOpenToggle, useToastHandler, useUserCookie };
