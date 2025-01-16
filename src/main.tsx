9aa98be58d34ff8617101ba090c04555bac95618
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-01-16 10:50:22 +0900

    git flow test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+import { initMocks } from '@/mocks/initMocks';
 import { router } from '@/Router/router';
 import { CookiesProvider } from 'react-cookie';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <CookiesProvider defaultSetOptions={{ path: '/' }}>
-    <RouterProvider router={router} />
-  </CookiesProvider>,
-);
+initMocks().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <CookiesProvider defaultSetOptions={{ path: '/' }}>
+      <RouterProvider router={router} />
+    </CookiesProvider>,
+  );
+});
