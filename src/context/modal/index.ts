16953ce82fbc1f07f140ac7c120a31faa14df2a2
jeonghee kim <heestory1992@gmail.com>
2025-01-16 21:36:18 +0900

    Merge: vercel 배포 레포지토리 변경사항 병합 (#154)
    * refactor: 사용되지 않는 내보내기 파일 제거
    
    * Create fork-sync.yml

--- src/context/modal/index.ts
+++ src/context/modal/index.ts
@@ -1,5 +1,4 @@
-import { modalContext } from '@/context/modal/context';
 import { ModalProvider } from '@/context/modal/provider';
 import { useModalContext } from '@/context/modal/useModalContext';
 
-export { modalContext, ModalProvider, useModalContext };
+export { ModalProvider, useModalContext };
