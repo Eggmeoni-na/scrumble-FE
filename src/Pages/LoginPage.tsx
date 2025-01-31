8b66ec21f0708ef1020e3640973061c5b48a37c9
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-01-31 19:55:00 +0900

    feat: 테스트용 로그인 버튼 추가

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,11 +1,14 @@
-import { getOAuthUrl } from '@/apis';
+import { generateTempSession, getOAuthUrl } from '@/apis';
 import { useUserCookie } from '@/hooks';
 import { pcMediaQuery } from '@/styles/breakpoints';
-import { css } from '@emotion/react';
-import { Navigate } from 'react-router-dom';
+import { AuthUser } from '@/types';
+import { css, Theme } from '@emotion/react';
+import { AxiosResponse } from 'axios';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  const { user } = useUserCookie();
+  const { user, setCookie } = useUserCookie();
+  const navigate = useNavigate();
 
   if (user) {
     return <Navigate to="/" replace />;
@@ -20,12 +23,43 @@ const LoginPage = () => {
     }
   };
 
+  const handleTempLogin = async () => {
+    try {
+      const response: AxiosResponse<{ data: AuthUser }> = await generateTempSession();
+      const { status, data } = response;
+      if (status === 200) {
+        const { id, name } = data.data;
+
+        setCookie(
+          'user',
+          { id, name },
+          {
+            path: '/',
+            sameSite: 'strict',
+          },
+        );
+
+        navigate('/squads', { replace: true });
+        return;
+      } else {
+        // TODO: 세션 발급 실패 에러 처리
+        console.error('Failed to generate session:', status);
+      }
+    } catch (error) {
+      // TODO: 로그인 실패 에러 안내
+      console.error('Failed to fetch Google OAuth URL', error);
+    }
+  };
+
   return (
     <div>
       <div css={loginContainer}>
         <img css={loginHomeImgStyle} src="/images/login.png" alt="login_image" />
         <button onClick={handleGoogleLogin} aria-label="Login with Google">
           <img css={googleLoginImgStyle} src="/images/google_login.png" alt="google_login" />
+        </button>
+        <button css={demoUserStyle} onClick={handleTempLogin}>
+          테스트 계정 로그인
         </button>
       </div>
     </div>
@@ -59,4 +93,10 @@ const googleLoginImgStyle = css`
   ${pcMediaQuery(css`
     height: 64px;
   `)}
+`;
+
+const demoUserStyle = (theme: Theme) => css`
+  color: ${theme.colors.text};
+  font-size: ${theme.typography.size_16};
+  text-decoration: underline;
 `;
