fc2db24fa55715a09b32cbe2945f4b6bfab19b37
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-02-10 21:55:13 +0900

    #191 feat: 닉네임 수정 기능 추가

--- src/Pages/MyPage.tsx
+++ src/Pages/MyPage.tsx
@@ -1,17 +1,22 @@
-import { deleteUser, logoutUser } from '@/apis';
+import { deleteUser, editNickname, logoutUser } from '@/apis';
+import { Edit } from '@/assets/icons';
+import { IconWrapper } from '@/components';
 import { Button } from '@/components/common';
 import { useToastHandler, useUserCookie } from '@/hooks';
 import { userQueries } from '@/hooks/queries';
 import { handleKeyDown } from '@/utils';
 import { css, Theme } from '@emotion/react';
 import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const MyPage = () => {
   const navigate = useNavigate();
-  const { clearCookie } = useUserCookie();
+  const { user: profile, setCookie, clearCookie } = useUserCookie();
   const { successToast, failedToast } = useToastHandler();
   const { data: user } = useSuspenseQuery(userQueries()).data;
+  const [newNickname, setNewNickname] = useState(profile?.name || '');
+  const [isEditNickname, setIsEditNickname] = useState(false);
   const { mutate: deleteUserMutate } = useMutation({
     mutationFn: deleteUser,
     onSuccess: () => {
@@ -20,6 +25,21 @@ const MyPage = () => {
     },
     onError: () => failedToast('잠시 후 다시 시도해주세요'),
   });
+
+  const handleEditNickname = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newNickname.trim() !== profile?.name) {
+      try {
+        const res = await editNickname(newNickname);
+        if (res.statusCodeValue === 200) {
+          setCookie('user', { id: profile?.id, name: newNickname });
+        }
+      } catch {
+        failedToast('닉네임 변경에 실패했어요');
+      }
+    }
+    setIsEditNickname(false);
+  };
 
   const handleLogout = async () => {
     const res = await logoutUser();
@@ -35,11 +55,42 @@ const MyPage = () => {
   return (
     <div css={containerStyle}>
       <h1>내 정보</h1>
-      <div css={userInfoStyle}>
-        <p>{user.email}</p>
-        <Button text="로그아웃" onClick={handleLogout} aria-label="로그아웃" css={logoutButtonStyle} />
-      </div>
-      <div css={deleteUserButtonStyle}>
+
+      <section css={userInfoStyle}>
+        <h3>계정</h3>
+        <div>
+          <p>{user.email}</p>
+          <Button text="로그아웃" onClick={handleLogout} aria-label="로그아웃" css={logoutButtonStyle} />
+        </div>
+      </section>
+
+      <section css={nicknameStyle}>
+        <h3>닉네임</h3>
+        {!isEditNickname ? (
+          <div>
+            <p>{profile?.name}</p>
+            <IconWrapper onClick={() => setIsEditNickname(true)} aria-label="닉네임 수정" style={{ width: '24px' }}>
+              <Edit />
+            </IconWrapper>
+          </div>
+        ) : (
+          <form css={editNicknameActionStyle} onSubmit={handleEditNickname}>
+            <input value={newNickname} onChange={(e) => setNewNickname(e.target.value)} autoFocus />
+            <Button text="수정" aria-label="닉네임 수정" style={{ backgroundColor: '#FFD700', color: '#616161' }} />
+            <Button
+              text="취소"
+              variant="default"
+              onClick={() => {
+                setNewNickname(profile?.name || '');
+                setIsEditNickname(false);
+              }}
+              aria-label="닉네임 수정 취소"
+            />
+          </form>
+        )}
+      </section>
+
+      <section css={deleteUserButtonStyle}>
         <span
           onClick={handleDeleteUser}
           onKeyDown={(e) => handleKeyDown(e, handleDeleteUser)}
@@ -48,7 +99,7 @@ const MyPage = () => {
         >
           회원탈퇴
         </span>
-      </div>
+      </section>
     </div>
   );
 };
@@ -70,7 +121,12 @@ const containerStyle = (theme: Theme) => css`
 
 const userInfoStyle = css`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  margin-bottom: 24px;
+
+  & div {
+    display: flex;
+  }
 
   & p {
     flex: 2;
@@ -80,9 +136,45 @@ const userInfoStyle = css`
 
 const logoutButtonStyle = (theme: Theme) => css`
   background-color: ${theme.colors.secondary};
-  color: white;
-
+  color: ${theme.colors.black.black300};
   flex: 0.5;
+`;
+
+const nicknameStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 24px;
+
+  & p {
+    height: 12px;
+  }
+
+  & div {
+    display: flex;
+    align-items: center;
+  }
+
+  & input {
+    width: 100%;
+    height: 40px;
+    max-width: 240px;
+    background-color: #fff;
+    border-radius: 6px;
+    margin: 16px 8px 16px 0;
+    padding: 0 8px;
+  }
+`;
+
+const editNicknameActionStyle = css`
+  display: flex;
+  align-items: center;
+
+  & button {
+    width: 72px;
+    height: 40px;
+    margin-right: 4px;
+  }
 `;
 
 const deleteUserButtonStyle = (theme: Theme) => css`
