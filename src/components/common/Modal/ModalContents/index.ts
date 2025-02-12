f83f96cdf26d40cf22223670062608fdcb6399f8
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-02-12 23:07:17 +0900

    #199 feat: 임시 로그인 모달 추가 및 ModalContents 내보내기 구조 수정

--- src/components/common/Modal/ModalContents/index.ts
+++ src/components/common/Modal/ModalContents/index.ts
@@ -1,2 +1,5 @@
-export { default as ActionModal } from './ActionModal';
-export { default as SquadForm } from './SquadForm';
+import ActionModal from '@/components/common/Modal/ModalContents/ActionModal';
+import EmailInputModal from '@/components/common/Modal/ModalContents/EmailInputModal';
+import { SquadForm } from '@/components/common/Modal/ModalContents/SquadForm';
+
+export { ActionModal, EmailInputModal, SquadForm };
