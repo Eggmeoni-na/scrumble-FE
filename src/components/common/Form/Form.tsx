6d5abf110173ec5b1e50469cc7eef2624b19f343
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-02-20 12:09:34 +0900

    fix: Form 컴포넌트의 style props 복구

--- src/components/common/Form/Form.tsx
+++ src/components/common/Form/Form.tsx
@@ -1,6 +1,6 @@
 import { Add } from '@/assets/icons';
 import { IconWrapper } from '@/components';
-import { css, Theme } from '@emotion/react';
+import { css, SerializedStyles, Theme } from '@emotion/react';
 import { ChangeEvent, FormEvent, HTMLInputTypeAttribute, KeyboardEvent } from 'react';
 
 type Props = {
@@ -10,10 +10,11 @@ type Props = {
   value: string | number;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
+  style?: (theme: Theme) => SerializedStyles;
 };
 
-const Form = ({ type, onSubmit, onKeyDown, value, onChange, placeholder }: Props) => (
-  <form css={formStyle} onSubmit={onSubmit}>
+const Form = ({ type, onSubmit, onKeyDown, value, onChange, placeholder, style }: Props) => (
+  <form css={[formStyle, style]} onSubmit={onSubmit}>
     <input type={type} value={value} onChange={onChange} autoFocus placeholder={placeholder} />
     <IconWrapper
       css={addIconStyle}
@@ -37,7 +38,6 @@ const formStyle = (theme: Theme) => css`
   background-color: ${theme.colors.background.gray};
   border-radius: 18px;
   padding: 8px;
-  margin: 16px;
 
   & input {
     height: 40px;
