e32ad474e0847f642904be5336b72d5c14292c9d
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-03-22 16:12:39 +0900

    refactor: useMemo로 스쿼드 리스트를 캐싱하여 불필요한 컴포넌트 리렌더링 방지

--- src/Pages/SquadPage/index.tsx
+++ src/Pages/SquadPage/index.tsx
@@ -5,12 +5,13 @@ import SquadItem from '@/Pages/SquadPage/SquadItem';
 import { blankStyle, createSquadButtonStyle, headerStyle } from '@/Pages/SquadPage/style';
 import { useToastStore } from '@/stores';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const SquadPage = () => {
   const navigate = useNavigate();
   const createToast = useToastStore((state) => state.createToast);
-  const { data: squadList } = useSuspenseQuery(squadQueryOptions()).data;
+  const { data } = useSuspenseQuery(squadQueryOptions()).data;
   const { CreateSquadModal, handleCreateSquad } = useCreateSquad({
     onSuccess: async ({ data }) => {
       navigate(`/squads/${data.squadId}`);
@@ -21,21 +22,15 @@ const SquadPage = () => {
     },
   });
 
+  const squadList = useMemo(() => data.map((squad) => <SquadItem key={squad.squadId} squad={squad} />), [data]);
+
   return (
     <>
       <div css={headerStyle}>
         <span>스쿼드</span>
         <Button text="스쿼드 생성" onClick={handleCreateSquad} customCss={createSquadButtonStyle} />
       </div>
-      {squadList.length ? (
-        <ul>
-          {squadList.map((squad) => (
-            <SquadItem key={squad.squadId} squad={squad} />
-          ))}
-        </ul>
-      ) : (
-        <EmptyContent message="참여중인 스쿼드가 없어요" css={blankStyle} />
-      )}
+      {squadList.length ? <ul>{squadList}</ul> : <EmptyContent message="참여중인 스쿼드가 없어요" css={blankStyle} />}
       <CreateSquadModal />
     </>
   );
