93f2fbb8dc94dfe6a1c0ad62175f60314cea4077
jeonghee kim <heehee2302@gmail.com>
2025-09-13 13:08:38 +0900

    Feat: 스쿼드 목록 아이템 UI 개선 (#240)
    * feat: root conatiner 그림자 구분 및 기본 배경색 변경
    
    * style: theme 스타일 수정
    
    * feat: 스쿼드 목록의 개별 아이템 UI 개선

--- src/components/common/ErrorBoundary/FallbackWrapper.tsx
+++ src/components/common/ErrorBoundary/FallbackWrapper.tsx
@@ -1,9 +1,11 @@
-import { Loading } from '@/components/common';
-import { ModalFallback } from '@/components/common/ErrorBoundary';
-import { MainLayout } from '@/components/layouts';
+import { css } from '@emotion/react';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 import { PropsWithChildren, Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+
+import { Loading } from '@/components/common';
+import { ModalFallback } from '@/components/common/ErrorBoundary';
+import { MainLayout } from '@/components/layouts';
 
 const FallbackWrapper = ({ children }: PropsWithChildren) => {
   const { reset } = useQueryErrorResetBoundary();
@@ -11,10 +13,20 @@ const FallbackWrapper = ({ children }: PropsWithChildren) => {
   return (
     <ErrorBoundary FallbackComponent={ModalFallback} onReset={reset}>
       <Suspense fallback={<Loading />}>
-        <MainLayout>{children}</MainLayout>
+        <div css={wrapperStyle}>
+          <MainLayout>{children}</MainLayout>
+        </div>
       </Suspense>
     </ErrorBoundary>
   );
 };
 
 export default FallbackWrapper;
+
+const wrapperStyle = css`
+  margin-left: 20px;
+  margin-right: 20px;
+  background-color: #fff;
+  border-radius: 12px;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.08);
+`;
