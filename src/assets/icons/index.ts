275d7f4d91c04c98ce900945ac36707f42c9fc0e
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2025-09-14 12:01:13 +0900

    add: 아이콘 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,12 @@
 import ActiveBell from './activeBell.svg?react';
 import Add from './add.svg?react';
+import ArrowLeft from './arrowLeft.svg?react';
+import ArrowRight from './arrowRight.svg?react';
 import Back from './back.svg?react';
 import Bell from './bell.svg?react';
 import Check from './check.svg?react';
 import Close from './close.svg?react';
+import Daily from './daily.svg?react';
 import Dark from './dark.svg?react';
 import Delete from './delete.svg?react';
 import Edit from './edit.svg?react';
@@ -11,20 +14,25 @@ import Exit from './exit.svg?react';
 import Failed from './failed.svg?react';
 import Light from './light.svg?react';
 import Menu from './menu.svg?react';
+import Monthly from './monthly.svg?react';
 import More from './more.svg?react';
 import Pouch from './pouch.svg?react';
 import Success from './success.svg?react';
 import Target from './target.svg?react';
 import User from './user.svg?react';
 import Warning from './warning.svg?react';
+import Weekly from './weekly.svg?react';
 
 export {
   ActiveBell,
   Add,
+  ArrowLeft,
+  ArrowRight,
   Back,
   Bell,
   Check,
   Close,
+  Daily,
   Dark,
   Delete,
   Edit,
@@ -32,10 +40,12 @@ export {
   Failed,
   Light,
   Menu,
+  Monthly,
   More,
   Pouch,
   Success,
   Target,
   User,
   Warning,
+  Weekly,
 };
